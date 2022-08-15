47388166661f2b420b60b9b0f38e5eb69cf51d6e
yejeungJi <jiyejeung@gmail.com>
2022-08-15 23:04:01 +0900

    Remark

---
+++ src/lecture_remark/LectureRemark.java
@@ -0,0 +1,25 @@
+package lecture_remark;
+
+/** 
+ * <pre>
+ * 클래스 주석
+ * </pre>
+ * @author : jiyejeung
+ * @date : 2022.08.15.
+ * */
+
+public class LectureRemark {
+
+	/** 
+	 * 메인 메소드
+	 * @author : jiyejeung
+	 * @date : 2022.08.15.
+	 * */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
