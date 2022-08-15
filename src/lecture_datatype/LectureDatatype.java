39ae583ad97bdf3f962d83bdd08f975fda942b4c
yejeungJi <jiyejeung@gmail.com>
2022-08-16 06:54:47 +0900

    Datatype

---
+++ src/lecture_datatype/LectureDatatype.java
@@ -0,0 +1,43 @@
+package lecture_datatype;
+
+public class LectureDatatype {
+
+	public static void main(String[] args) {
+		
+		char charOne = '안';
+		char charTwo = '녕';
+		
+		System.out.println(charOne);
+		System.out.println(charTwo);
+		
+		String strOne = "안녕하세요";
+		
+		System.out.println(strOne);
+		
+		byte byteOne = 1;
+		
+		System.out.println(byteOne);
+		
+		short shortOne = 2;
+		
+		System.out.println(shortOne);
+		
+		int intOne = 3;
+		
+		System.out.println(intOne);
+		
+		long longOne = 4;
+		
+		System.out.println(longOne);
+		
+		float floatOne = 0.1f;
+		
+		System.out.println(floatOne);
+		
+		double doubleOne = 0.2;
+		
+		System.out.println(doubleOne);
+		
+	}
+
+}
