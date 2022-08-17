b108a3a778ba8e4c3661991f1c844612be080cab
yejeungJi <jiyejeung@gmail.com>
2022-08-17 23:53:47 +0900

    Loop

---
+++ src/lecture_loop/Lecture_Loop.java
@@ -0,0 +1,15 @@
+package lecture_loop;
+
+public class Lecture_Loop {
+
+	public static void main(String[] args) {
+		
+		int arrInt[] = new int[] { 1, 2, 3, 4, 5 };
+		
+		for (int i = 0; i < arrInt.length; i++) {
+			System.out.println(arrInt[i]);
+		}
+		
+	}
+
+}
